86abb498532b5fdefd2d954558a77124a7923453
Stuuu <stuartschrick@gmail.com>
2019-07-10 20:45:03 -0400

    vars

---
+++ herewego.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Hello World")
+
+	var age int = 40
+
+	var favNum float64 = 1.6180339 
+
+
+	fmt.Println(age, favNum)
+
+	var numOne = 1.0000
+
+	var num99 = .9999
+
+	fmt.Println(numOne - num99)
+}
